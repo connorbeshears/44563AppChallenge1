c3b98f86390b24c862009698e031acbb84b4f823
Connor Beshears <connorbeshears@gmail.com>
2019-09-30 12:32:43 -0500

    fixed testing

--- tests.js
+++ tests.js
@@ -1,3 +1,12 @@
+
+function calculate(x, y){
+    return(x * y)
+}
+
 QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
+})
+
+QUnit.test("calc test", function(assert){
+    assert.ok(calculate(5,5) == 25, "Passed")
 })
