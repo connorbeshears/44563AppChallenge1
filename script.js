430cb6d35c31fe9c4fb54dc37d499f82780873de
Connor Beshears <connorbeshears@gmail.com>
2019-09-30 12:26:53 -0500

    added testable function

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+function calculate(x, y){
+    return(x * y)
+}
+
 function doCalc(){
     const things = parseFloat(document.querySelector("#string1").value)
     const size = parseFloat(document.querySelector("#string2").value)
 
-    total = things * size
+    total = calculate(things, size);
 
 
     document.querySelector('#shortString').innerHTML =  total
